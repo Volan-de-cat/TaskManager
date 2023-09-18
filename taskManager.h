17797bb7015a2b104149b43b3c565fa7cb4026ee
Volan-de-cat <69093126+Volan-de-cat@users.noreply.github.com>
2023-09-18 13:36:17 +0300

    Update taskManager.h

--- taskManager.h
+++ taskManager.h
@@ -8,10 +8,9 @@ public:
     void addTask(const Task& task);
     void addTask(Task&& eDate);
     void modifyTask(int mode, std::string word, std::vector<Task>::iterator it);
-    void modifyTask(int mode, std::string word, std::vector<Task>* search);
     void deleteTask(std::vector<Task>::iterator it);
     std::vector<Task>::iterator searchFirstTask(int mode, std::string word);
-    void searchTasks(int mode, std::string word, std::vector<Task>* search);
+    std::vector<std::vector<Task>::iterator> searchTasks(int mode, std::string word);
     void Save();
     void Load();
 
